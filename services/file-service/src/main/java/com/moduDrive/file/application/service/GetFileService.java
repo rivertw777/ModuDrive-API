a560083f0f50a0fcd4f3f47af32dedc6393ecc00
rivertw777 <rivertw7@gmail.com>
2026-07-14 12:42:27 +0900

    feat(file): add get file API (GET /api/v1/files/{fileId})
    Closes #59

---
+++ services/file-service/src/main/java/com/moduDrive/file/application/service/GetFileService.java
@@ -0,0 +1,31 @@
+package com.moduDrive.file.application.service;
+
+import com.moduDrive.common.core.annotation.UseCase;
+import com.moduDrive.common.core.exception.BusinessException;
+import com.moduDrive.file.application.port.in.command.GetFileCommand;
+import com.moduDrive.file.application.port.in.usecase.GetFileUseCase;
+import com.moduDrive.file.application.port.out.FindFilePort;
+import com.moduDrive.file.domain.model.File;
+import com.moduDrive.file.domain.model.FileStatus;
+import com.moduDrive.file.exception.FileExceptionCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+@UseCase
+@RequiredArgsConstructor
+class GetFileService implements GetFileUseCase {
+
+    private final FindFilePort findFilePort;
+
+    @Transactional(readOnly = true)
+    @Override
+    public File getFile(GetFileCommand command) {
+        File file = findFilePort.findById(command.getFileId())
+                .orElseThrow(() -> new BusinessException(FileExceptionCase.FILE_NOT_FOUND));
+
+        if (file.getStatus() == FileStatus.DELETED) {
+            throw new BusinessException(FileExceptionCase.FILE_ALREADY_DELETED);
+        }
+        return file;
+    }
+}
